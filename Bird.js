815b009847874d0a41ad0e9e9d54994d37bcaa62
DeniseOdeth18 <89421213+DeniseOdeth18@users.noreply.github.com>
2021-11-08 12:52:23 -0600

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,28 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+
+    //smoke image
+    
+    //trajectory
+    
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+
+    //condicion
+    //if(){
+
+      //var position y trajectory
+    
+    //}
+    
+    for(var i=0; i<this.trajectory.length; i++){
+      
+    }
+  }
+}
---
+++ script.js
@@ -0,0 +1,71 @@
+//Ejemplos de los diferentes tipos de numeros en JavaScript
+
+//Cadena
+var string = "Esta es una cadena";
+console.log(string);
+
+//Numero
+var num = 100;
+console.log(num);
+
+//Booleano
+var bool = true;
+console.log(bool);
+
+//Indefinido
+var object;
+console.log(object);
+
+//Reasignando el mismo objeto indefinido a nulo
+
+//Nulo
+object = null;
+console.log(object);
+
+//Ejemplos de matrices
+
+//Una matriz que contiene el mismo tipo de datos
+var arr1 = [1,2,3,4,5];
+console.log(arr1);
+
+var arr2 = ["nombre",12,true];
+console.log(arr2);
+
+//Una matriz que almacena una lista de matrices
+var arr3 = [[1,2], [2,3], [3,4]];
+console.log(arr3);
+
+//Acceder al primer elemento de la matriz
+console.log(arr3[0]);
+
+//Acceder al segundo elemento de la matriz
+console.log(arr3[0][1]);
+
+arr3.push("mi nombre");
+console.log(arr3);
+
+arr3.pop();
+console.log(arr3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
